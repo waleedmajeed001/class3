680ec327096b9cb8015eefb5f6be29375a2b5f2e
waleedmajeed001 <147373175+waleedmajeed001@users.noreply.github.com>
2024-03-25 17:21:15 +0500

    class3

---
+++ src/app/blog/address/page.tsx
@@ -0,0 +1,4 @@
+function address() {
+    return <h1>This is my frist blog</h1>
+  }
+  export default address;
---
+++ src/app/blog/page.tsx
@@ -0,0 +1,18 @@
+
+'use client';
+
+import { useRouter } from 'next/navigation';
+
+export default function GiveName() {
+  const router = useRouter();
+    return (
+      <div>
+            My name is Waleed Majeed.
+            <br/>
+            <button type="button" onClick={() => router.push('/name/address')}>
+              Get Address
+            </button>
+      
+      </div>
+    )
+  }
